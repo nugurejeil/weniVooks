79cf86a36252d209d03cee9557477fee008604fb
chimaek <pipiru100@gmail.com>
2025-07-04 09:03:11 +0900

    위니북스 추가가

---
+++ src/app/cursor-with-python/bookInfo.js
@@ -0,0 +1,9 @@
+export const DEFAULT_PATH = 'cursor-with-python';
+export const TITLE = 'Cursor 톺아보기 with Python';
+export const DESCRIPTION = 'Cursor IDE와 함께 파이썬을 쉽고 재미있게! AI 협업으로 코딩을 배우며 게임 개발부터 웹 개발까지 실전 프로젝트로 학습해 보세요.';
+
+export const COVER = `/images/${DEFAULT_PATH}/cover.png`; // 책 표지
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`; // 링크 공유 시 나올 이미지
+
+// 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
+export const EDITOR = ['HTML/CSS','Python'];
