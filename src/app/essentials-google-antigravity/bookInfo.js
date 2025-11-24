c4ab1cdd88608a5aac05b4349975d9afde0c270f
chimaek <pipiru100@gmail.com>
2025-11-24 10:38:40 +0900

    ADD

---
+++ src/app/essentials-google-antigravity/bookInfo.js
@@ -0,0 +1,9 @@
+export const DEFAULT_PATH = 'essentials-google-antigravity';
+export const TITLE = 'google-antigravity 에센셜';
+export const DESCRIPTION = '위니브의 다양한 교안을 웹에서 확인해보세요!';
+
+export const COVER = `/images/${DEFAULT_PATH}/cover.png`; // 책 표지
+export const OG_IMG = `/images/${DEFAULT_PATH}/cover.png`; // 링크 공유 시 나올 이미지
+
+// 'HTML/CSS', 'Python', 'JavaScript' 중 선택하여 삽입해주세요
+export const EDITOR = ['HTML/CSS'];
